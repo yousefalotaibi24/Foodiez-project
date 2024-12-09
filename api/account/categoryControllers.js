f8f43f151baa7dae5373e85e29ed580572f02988
yousefalotaibi24 <yousefalotaibi21@gmail.com>
2024-12-09 22:27:27 +0300

    Yousef Controllers done

--- api/account/categoryControllers.js
+++ api/account/categoryControllers.js
@@ -1,1 +1,49 @@
-// with yosif
+// with yousef
+
+const Category = require("../models/Category");
+exports.listCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+exports.categoryDetail = async (req, res) => {
+  const { categoryId } = req.params;
+  const category = await Category.findById(categoryId);
+  if (category) {
+    res.status(200).json(category);
+  } else {
+    res.status(404).json();
+  }
+};
+
+exports.createCategory = async (req, res) => {
+  const category = new Category(req.body);
+  const savedCategory = await category.save();
+  res.status(201).json(savedCategory);
+};
+
+exports.updateCategory = async (req, res) => {
+  const { categoryId } = req.params;
+  const foundCategory = await Category.findById(categoryId);
+  if (foundCategory) {
+    await foundCategory.updateOne(req.body);
+    res.status(202).json();
+  } else {
+    res.status(404).json();
+  }
+};
+
+exports.deleteCategory = async (req, res) => {
+  const { categoryId } = req.params;
+  const foundCategory = await Category.findById(categoryId);
+  if (foundCategory) {
+    await foundCategory.deleteOne();
+    res.status(204).end();
+  } else {
+    res.status(404).json();
+  }
+};
--- api/account/ingredientsControllers.js
+++ api/account/ingredientsControllers.js
@@ -1,1 +1,45 @@
-// with yosif
+// with yousef
+const e = require("express");
+const Ingredients = require("../models/Ingredients");
+exports.listIngredients = async (req, res) => {
+  try {
+    const ingredients = await Ingredients.find();
+    res.status(200).json(ingredients);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+exports.ingredientDetail = async (req, res) => {
+  const { ingredientId } = req.params;
+  const ingredient = await Ingredients.findById(ingredientId);
+  if (ingredient) {
+    res.status(200).json(ingredient);
+  } else {
+    res.status(404).json();
+  }
+};
+exports.createIngredient = async (req, res) => {
+  const ingredient = new Ingredients(req.body);
+  const savedIngredient = await ingredient.save();
+  res.status(201).json(savedIngredient);
+};
+exports.updateIngredient = async (req, res) => {
+  const { ingredientId } = req.params;
+  const foundIngredient = await Ingredients.findById(ingredientId);
+  if (foundIngredient) {
+    await foundIngredient.updateOne(req.body);
+    res.status(202).json();
+  } else {
+    res.status(404).json();
+  }
+};
+exports.deleteIngredient = async (req, res) => {
+  const { ingredientId } = req.params;
+  const foundIngredient = await Ingredients.findById(ingredientId);
+  if (foundIngredient) {
+    await foundIngredient.deleteOne();
+    res.status(204).end();
+  } else {
+    res.status(404).json();
+  }
+};
