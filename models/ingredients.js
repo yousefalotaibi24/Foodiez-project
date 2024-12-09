67dff397c096824016f8cb05e976666ab642fd83
yousefalotaibi24 <yousefalotaibi21@gmail.com>
2024-12-09 20:54:07 +0300

    ingredients

---
+++ models/ingredients.js
@@ -0,0 +1,9 @@
+const { model, Schema } = require("mongoose");
+
+const ingredientsSchema = new Schema({
+  name: String,
+  scale: String,
+  amount: String,
+});
+
+module.exports = model("ingredients", ingredientsSchema);
