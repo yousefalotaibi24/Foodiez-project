6ebbb7b5b2cd79648c7cce7db87617ab2b85bd80
MrHasak99 <hamad_alkhalaf_99@hotmail.com>
2024-12-09 20:21:34 +0300

    Setup Mongoose

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const connectDb = require("./database");
 
 const app = express();
 
+connectDb();
 
 app.listen(8000, () => {
-    console.log("The application is running on localhost:8000");
-  });
+  console.log("The application is running on localhost:8000");
+});
