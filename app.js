4bd42b83a1c13f458ec37e88298ea78a4233fafe
Abdullah Abbas <sl_amg@msn.com>
2024-12-09 20:26:54 +0300

    updated app.js

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const connectDb = require("./database");
-
 const app = express();
+const port = 8000;
+const path = require("path");
 
+
+app.use(express.json());
+app.use('/media', 
+  express.static(path.join(__dirname, 'media')) 
+); 
+app.use("/accounts", accountsRouter)
 connectDb();
 
-app.listen(8000, () => {
-  console.log("The application is running on localhost:8000");
+app.listen(port, () => {
+  console.log(`This is to test if my server is working at port: ${port}`);
 });
