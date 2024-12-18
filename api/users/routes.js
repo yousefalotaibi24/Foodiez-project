e351b8fea306b36575f589f7c7ecc942c6c8d61e
yousefalotaibi24 <yousefalotaibi21@gmail.com>
2024-12-18 06:19:08 +0300

    fixs bugs

--- api/users/routes.js
+++ api/users/routes.js
@@ -7,3 +7,5 @@ const { registerUser, loginUser, logoutUser } = require("./controllers");
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+
+module.exports = router
